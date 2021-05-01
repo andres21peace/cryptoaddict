3101edc909e78765b95949033352351b4f2c98ac
andres21peace <65026338+andres21peace@users.noreply.github.com>
2021-05-01 15:30:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const coinInput = document.getElementById('coin');
+const currencyInput = document.getElementById('curr');
+const submit = document.getElementById('form');
+let request = new XMLHttpRequest();
+const result = document.getElementById('result');
+
+submit.addEventListener('submit', ($event) =>{
+  $event.preventDefault();
+  
+  const coinValue = coinInput.value;
+  const currValue = currencyInput.value;
+  request.open('GET', 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=' + currValue +'&ids='+ coinValue + '&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  request.send();
+  
+})
+
+request.onreadystatechange = () => {
+  if(request.readyState === 4){
+    let response = JSON.parse(request.response);
+    console.log(response);
+   
+    if(currencyInput.value == "eur"){
+          result.textContent = "The price of " + response[0].id + ' is currently € ' + response[0].ath;
+    }else if(currencyInput.value == "usd"){
+          result.textContent = "The price of " + response[0].id + ' is currently $' + response[0].ath;
+    }else{
+          result.textContent = "The price of " + response[0].id + ' is currently' + response[0].ath;
+    }
+  }
+    
+}
